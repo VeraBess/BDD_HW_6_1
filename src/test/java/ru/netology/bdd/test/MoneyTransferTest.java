04904f84b87440e0fadc7c209a783da44f820eb5
vera bessonova <v2112365@gmail.com>
2025-06-15 11:07:33 +0300

    Изменено расположение кода в пакетах

--- src/test/java/ru/netology/bdd/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/bdd/test/MoneyTransferTest.java
@@ -3,38 +3,36 @@ package ru.netology.bdd.test;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ru.netology.bdd.data.DataHelper;
 import ru.netology.bdd.page.DashboardPage;
 import ru.netology.bdd.page.LoginPage;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static ru.netology.bdd.data.DataHelper.generateInvalidAmount;
-import static ru.netology.bdd.data.DataHelper.generateValidAmount;
+
 
 public class MoneyTransferTest {
 
     DashboardPage dashboardPage;
-    DataHelper.CardInfo firstCardInfo;
-    DataHelper.CardInfo secondCardInfo;
+    ru.netology.bdd.data.DataHelper.CardInfo firstCardInfo;
+    ru.netology.bdd.data.DataHelper.CardInfo secondCardInfo;
     int firstCardBalance;
     int secondCardBalance;
 
     @BeforeEach
     void setup() {
         var loginPage = Selenide.open("http://localhost:9999", LoginPage.class);
-        var authInfo = DataHelper.getAuthInfo();
+        var authInfo = ru.netology.bdd.data.DataHelper.getAuthInfo();
         var verificationPage = loginPage.validLogin(authInfo);
-        var verificationCode = DataHelper.getVerificationCode();
+        var verificationCode = ru.netology.bdd.data.DataHelper.getVerificationCode();
         dashboardPage = verificationPage.validVerify(verificationCode);
-        firstCardInfo = DataHelper.getFirstCardInfo();
-        secondCardInfo = DataHelper.getSecondCardInfo();
+        firstCardInfo = ru.netology.bdd.data.DataHelper.getFirstCardInfo();
+        secondCardInfo = ru.netology.bdd.data.DataHelper.getSecondCardInfo();
         firstCardBalance = dashboardPage.getCardBalance(firstCardInfo);
         secondCardBalance = dashboardPage.getCardBalance(secondCardInfo);
     }
 
     @Test
     void shouldTransferFromFirstToSecond() {
-        var amount = generateValidAmount(firstCardBalance);
+        var amount = ru.netology.bdd.data.DataHelper.generateValidAmount(firstCardBalance);
         var expectedBalanceFirstCard = firstCardBalance - amount;
         var expectedBalanceSecondCard = secondCardBalance + amount;
         var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo);
